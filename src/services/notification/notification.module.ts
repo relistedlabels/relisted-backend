81a1c82c25add94a37a9f0ed5ea7d5a17a2947cf
willythecruise <emmanuelwilliam49@gmail.com>
2026-03-14 19:58:35 +0100

    brands fix

--- src/services/notification/notification.module.ts
+++ src/services/notification/notification.module.ts
@@ -2,12 +2,13 @@ import { Global, Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { MailModule } from '../mail/mail.module';
 import { NotificationController } from './notification.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Global()
 @Module({
   imports: [MailModule],
   controllers: [NotificationController],
-  providers: [NotificationService],
+  providers: [NotificationService, PrismaService],
   exports: [NotificationService],
 })
  export class NotificationModule {}
